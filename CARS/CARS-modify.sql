2927645d55442cdda499a2f44aff41e186bdc350
Abhinav Singh <abhinav.s1997@yahoo.com>
2018-10-17 22:20:47 -0700

    Update CARS-modify.sql

--- CARS/CARS-modify.sql
+++ CARS/CARS-modify.sql
@@ -2,7 +2,7 @@
 #asingh54@calpoly.edu and 
 
 -- Removes from car data all cars that are not 1979 or 1980 and if they are they have to have more than 20 MPG. Then it removes any cars less than 26 MPG and any cars with less than 110 horsepower. Finally any car with less than 8 cylinders or more than 10s of accelerations is removed.
-DELETE FROM carData WHERE ((Year != 1979 OR Year != 1980) OR MPG < 20) AND (MPG < 26 AND Horsepower < 110) AND (Cylinders < 8 OR Accelerate >= 10);
+DELETE FROM carData WHERE ((Year != 1979 OR Year != 1980) OR MPG < 20) AND (MPG < 26 OR Horsepower < 110) AND (Cylinders < 8 OR Accelerate >= 10);
 -- Select statement to see the data
 SELECT * FROM carData ORDER BY Year, Id;
 --Removes the specified fields
