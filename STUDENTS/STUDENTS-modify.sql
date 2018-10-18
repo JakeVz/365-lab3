2ec48420ae8a0e1bc05fe822a1740ca065a75110
JakeVz <jakevz122@gmail.com>
2018-10-17 19:17:49 -0700

    need your email

--- STUDENTS/STUDENTS-modify.sql
+++ STUDENTS/STUDENTS-modify.sql
@@ -7,7 +7,7 @@ DELETE FROM list WHERE grade != 5 AND grade != 6;
 
 INSERT INTO teachers(LastName, FirstName, Classroom) VALUES ('GAWAIN', 'AP-MAWR', 120);
 
-UPDATE list SET Classroom = 120 WHERE LastName = 'FLACHS' AND FirstName = 'JEFFREY';
+UPDATE list SET Classroom = 120 WHERE LastName = 'FLACHS' AND FirstName = 'JEFFRY';
 UPDATE list SET Classroom = 120 WHERE LastName = 'HUANG' AND FirstName = 'TAWANNA';
 UPDATE list SET Classroom = 120 WHERE LastName = 'GRUNIN' AND FirstName = 'EMILE';
 
@@ -19,7 +19,7 @@ UPDATE list SET GPA = 3.5 WHERE Grade = 5 AND Classroom = 120;
 UPDATE list SET GPA = 4.0 WHERE LastName = 'MACIAH' AND FirstName = 'CHET';
 UPDATE list SET GPA = GPA + 0.3 WHERE LastName = 'GERSTEIN' AND FirstName = 'AL';
 UPDATE list SET GPA = GPA * 1.10 WHERE LastName = 'HUANG' AND FirstName = 'TAWANNA';
-UPDATE list SET GPA = GPA * 1.10 WHERE LastName = 'JAGNEUX' AND FirstName = 'ELVIRA';
+UPDATE list SET GPA = GPA * 1.10 WHERE LastName = 'JAGNEAUX' AND FirstName = 'ELVIRA';
 
 
 SELECT * FROM list ORDER BY GPA, grade, LastName;
